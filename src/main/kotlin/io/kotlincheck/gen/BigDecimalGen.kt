b1ae768a6bcd91a9061caed2adc9dc182bff8bc4
misha <chemike47@gmail.com>
2017-05-23 22:43:16 +0300

    Use BigDecimalShrinker/BigIntegerShrinker in BigDecimalGen/BigIntegerGen

--- src/main/kotlin/io/kotlincheck/gen/BigDecimalGen.kt
+++ src/main/kotlin/io/kotlincheck/gen/BigDecimalGen.kt
@@ -1,13 +1,14 @@
 package io.kotlincheck.gen
 
 import io.kotlincheck.Random
+import io.kotlincheck.shrink.BigDecimalShrinker
 import io.kotlincheck.shrink.Shrinker
 import java.math.BigDecimal
 
 class BigDecimalGen(
         val origin: BigDecimal,
         val bound: BigDecimal,
-        shrinker: Shrinker<BigDecimal>? = null
+        shrinker: Shrinker<BigDecimal>? = BigDecimalShrinker(origin, bound)
 ) : Gen<BigDecimal>(shrinker) {
     override fun generate(): BigDecimal = Random.nextBigDecimal(origin, bound)
 }
--- src/main/kotlin/io/kotlincheck/gen/BigIntegerGen.kt
+++ src/main/kotlin/io/kotlincheck/gen/BigIntegerGen.kt
@@ -1,13 +1,14 @@
 package io.kotlincheck.gen
 
 import io.kotlincheck.Random
+import io.kotlincheck.shrink.BigIntegerShrinker
 import io.kotlincheck.shrink.Shrinker
 import java.math.BigInteger
 
 class BigIntegerGen(
         val origin: BigInteger,
         val bound: BigInteger,
-        shrinker: Shrinker<BigInteger>? = null
+        shrinker: Shrinker<BigInteger>? = BigIntegerShrinker(origin, bound)
 ) : Gen<BigInteger>(shrinker) {
     override fun generate(): BigInteger = Random.nextBigInteger(origin, bound)
 }
