9c6f1ce0bae3ba1c38f37ee7c308ea64e3272e5a
misha <chemike47@gmail.com>
2017-05-24 05:37:44 +0300

    Cleanup

--- src/test/kotlin/io/kotlincheck/shrink/MutableListShrinkerSpec.kt
+++ src/test/kotlin/io/kotlincheck/shrink/MutableListShrinkerSpec.kt
@@ -1,19 +1,20 @@
 package io.kotlincheck.shrink
 
 import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.ShouldSpec
 
-class MutableListShrinkerSpec : io.kotlintest.specs.ShouldSpec() {
+class MutableListShrinkerSpec : ShouldSpec() {
     init {
         "MutableListShrinker<Int>.shrink"{
             should("return `emptyList<Int>` if `emptyList<Int>` is counterexample") {
-                val shrinker = io.kotlincheck.shrink.MutableListShrinker(io.kotlincheck.shrink.IntShrinker(-100, 100), 5, false)
+                val shrinker = MutableListShrinker(IntShrinker(-100, 100), 5, false)
                 val counterexample = mutableListOf(-1, 2, -3, 4, -5)
                 shrinker.shrink({ false }, counterexample) shouldBe emptyList<Int>()
             }
 
             should("return a list of zeros of the same size if size fixed " +
                    "and the list is counterexample") {
-                val shrinker = io.kotlincheck.shrink.MutableListShrinker(io.kotlincheck.shrink.IntShrinker(-100, 100), 5, true)
+                val shrinker = MutableListShrinker(IntShrinker(-100, 100), 5, true)
                 val counterexample = mutableListOf(-1, 2, -3, 4, -5)
                 shrinker.shrink({ false }, counterexample) shouldBe List(counterexample.size) { 0 }
             }
