69ed0934c15ffa45bacc49e1fccf86188e005e6b
misha <chemike47@gmail.com>
2017-05-31 10:53:18 +0300

    Proper handling recursive data type in `Arbitrary.default`

--- src/main/kotlin/io/kotlincheck/arbitrary/Arbitrary.kt
+++ src/main/kotlin/io/kotlincheck/arbitrary/Arbitrary.kt
@@ -8,6 +8,7 @@ import kotlin.reflect.KClass
 import kotlin.reflect.KFunction
 import kotlin.reflect.full.isSuperclassOf
 import kotlin.reflect.full.withNullability
+import kotlin.reflect.jvm.jvmErasure
 
 open class Arbitrary<T>(
         val gen: Gen<T>,
@@ -17,6 +18,7 @@ open class Arbitrary<T>(
     @Suppress("UNCHECKED_CAST")
     companion object {
         val DEFAULT_CONTAINER_SIZE_BOUND = 100
+        val DEFAULT_MAX_RECURSION_DEPTH = 5
 
         inline fun <reified T> default(): Arbitrary<T> = when {
             Collection::class.isSuperclassOf(T::class) -> {
@@ -49,32 +51,40 @@ open class Arbitrary<T>(
             else -> forClassName(T::class.qualifiedName!!) as Arbitrary<T>
         }
 
-        fun forClassName(className: String, recursionDeep: Int = 0): Arbitrary<*>? {
-            if (recursionDeep > 5) {
-                return null
-            }
-
-            return when (className) {
-                "java.lang.Long", "kotlin.Long" -> LongArbitrary()
-                "java.lang.Integer", "kotlin.Int" -> IntArbitrary()
-                "java.lang.Short", "kotlin.Short" -> ShortArbitrary()
-                "java.lang.Byte", "kotlin.Byte" -> ByteArbitrary()
-                "java.lang.Char", "kotlin.Char" -> CharArbitrary()
-                "java.lang.Boolean", "kotlin.Boolean" -> BooleanArbitrary()
-                "java.lang.Double", "kotlin.Double" -> DoubleArbitrary()
-                "java.lang.Float", "kotlin.Float" -> FloatArbitrary()
-                "java.lang.String", "kotlin.String" -> StringArbitrary()
-                else -> forArbitraryKClass(Class.forName(className).kotlin, recursionDeep)
-            }
+        fun forClassName(
+                className: String,
+                usedClasses: Set<KClass<*>> = setOf(),
+                recursionDeep: Int = 0
+        ): Arbitrary<*>? = when (className) {
+            "java.lang.Long", "kotlin.Long" -> LongArbitrary()
+            "java.lang.Integer", "kotlin.Int" -> IntArbitrary()
+            "java.lang.Short", "kotlin.Short" -> ShortArbitrary()
+            "java.lang.Byte", "kotlin.Byte" -> ByteArbitrary()
+            "java.lang.Char", "kotlin.Char" -> CharArbitrary()
+            "java.lang.Boolean", "kotlin.Boolean" -> BooleanArbitrary()
+            "java.lang.Double", "kotlin.Double" -> DoubleArbitrary()
+            "java.lang.Float", "kotlin.Float" -> FloatArbitrary()
+            "java.lang.String", "kotlin.String" -> StringArbitrary()
+            else -> forArbitraryKClass(Class.forName(className).kotlin, usedClasses, recursionDeep)
         }
 
-        fun <T : Any> forArbitraryKClass(klass: KClass<T>, recursionDeep: Int = 0): Arbitrary<T> {
+        fun <T : Any> forArbitraryKClass(
+                klass: KClass<T>,
+                usedClasses: Set<KClass<*>>,
+                recursionDepth: Int = 0
+        ): Arbitrary<T> {
             fun <T> forConstructor(constructor: KFunction<T>): Arbitrary<T>? {
                 val paramArbs = constructor.parameters
                         .map { it.type }
                         .map { it.withNullability(false) }
-                        .map { it.toString() }
-                        .map { forClassName(it, recursionDeep + 1) }
+                        .map { it.jvmErasure }
+                        .map {
+                            if (recursionDepth < DEFAULT_MAX_RECURSION_DEPTH || it !in usedClasses) {
+                                forClassName(it.qualifiedName!!, usedClasses + it, recursionDepth + 1)
+                            } else {
+                                null
+                            }
+                        }
                 if (paramArbs.contains(null)) {
                     return null
                 }
