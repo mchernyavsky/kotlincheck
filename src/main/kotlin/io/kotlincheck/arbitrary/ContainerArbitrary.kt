1fd03aa37bf2b1b365e3a21024620e5827bc38d8
mchernyavsky <chemike47@gmail.com>
2017-06-01 21:57:13 +0300

    Add factory methods

--- src/main/kotlin/io/kotlincheck/arbitrary/ContainerArbitrary.kt
+++ src/main/kotlin/io/kotlincheck/arbitrary/ContainerArbitrary.kt
@@ -7,15 +7,6 @@ import io.kotlincheck.gen.StringGen
 import io.kotlincheck.shrink.MutableListShrinker
 import io.kotlincheck.shrink.StringShrinker
 
-class ListArbitrary<T>(
-        arb: Arbitrary<T>,
-        sizeBound: Int = Arbitrary.DEFAULT_CONTAINER_SIZE_BOUND,
-        fixedSize: Boolean = false
-) : Arbitrary<MutableList<T>>(
-        MutableListGen(arb, sizeBound, fixedSize),
-        MutableListShrinker(arb, sizeBound, fixedSize)
-)
-
 class MutableListArbitrary<T>(
         arb: Arbitrary<T>,
         sizeBound: Int = Arbitrary.DEFAULT_CONTAINER_SIZE_BOUND,
@@ -25,24 +16,11 @@ class MutableListArbitrary<T>(
         MutableListShrinker(arb, sizeBound, fixedSize)
 )
 
-class SetArbitrary<T>(
-        arb: Arbitrary<T>,
-        sizeBound: Int = Arbitrary.DEFAULT_CONTAINER_SIZE_BOUND,
-        fixedSize: Boolean = false
-) : Arbitrary<Set<T>>(MutableSetGen(arb, sizeBound, fixedSize))
-
 class MutableSetArbitrary<T>(
         arb: Arbitrary<T>,
         sizeBound: Int = Arbitrary.DEFAULT_CONTAINER_SIZE_BOUND,
         fixedSize: Boolean = false
 ) : Arbitrary<MutableSet<T>>(MutableSetGen(arb, sizeBound, fixedSize))
-
-class MapArbitrary<K, V>(
-        keyArb: Arbitrary<K>,
-        valueArb: Arbitrary<V>,
-        sizeBound: Int = Arbitrary.DEFAULT_CONTAINER_SIZE_BOUND,
-        fixedSize: Boolean = false
-) : Arbitrary<Map<K, V>>(MutableMapGen(keyArb, valueArb, sizeBound, fixedSize))
 
 class MutableMapArbitrary<K, V>(
         keyArb: Arbitrary<K>,
@@ -58,3 +36,29 @@ class StringArbitrary(
         StringGen(sizeBound, fixedSize),
         StringShrinker(sizeBound, fixedSize)
 )
+
+/** API */
+
+inline fun <reified T> lists(
+        values: Arbitrary<T> = Arbitrary.default<T>(),
+        sizeBound: Int = Arbitrary.DEFAULT_CONTAINER_SIZE_BOUND,
+        fixedSize: Boolean = false
+): MutableListArbitrary<T> = MutableListArbitrary<T>(values, sizeBound = sizeBound, fixedSize = fixedSize)
+
+inline fun <reified T> sets(
+        values: Arbitrary<T> = Arbitrary.default<T>(),
+        sizeBound: Int = Arbitrary.DEFAULT_CONTAINER_SIZE_BOUND,
+        fixedSize: Boolean = false
+): MutableSetArbitrary<T> = MutableSetArbitrary<T>(values, sizeBound = sizeBound, fixedSize = fixedSize)
+
+inline fun <reified K, reified V> maps(
+        keys: Arbitrary<K> = Arbitrary.default<K>(),
+        values: Arbitrary<V> = Arbitrary.default<V>(),
+        sizeBound: Int = Arbitrary.DEFAULT_CONTAINER_SIZE_BOUND,
+        fixedSize: Boolean = false
+): MutableMapArbitrary<K, V> = MutableMapArbitrary<K, V>(keys, values, sizeBound = sizeBound, fixedSize = fixedSize)
+
+inline fun strings(
+        sizeBound: Int = Arbitrary.DEFAULT_CONTAINER_SIZE_BOUND,
+        fixedSize: Boolean = false
+): StringArbitrary = StringArbitrary(sizeBound = sizeBound, fixedSize = fixedSize)
--- src/main/kotlin/io/kotlincheck/arbitrary/PrimitiveArbitrary.kt
+++ src/main/kotlin/io/kotlincheck/arbitrary/PrimitiveArbitrary.kt
@@ -44,25 +44,82 @@ class FloatArbitrary(
 ) : Arbitrary<Float>(FloatGen(origin, bound), FloatShrinker(origin, bound))
 
 class BigDecimalArbitrary(
-        val origin: BigDecimal,
-        val bound: BigDecimal
+        val origin: BigDecimal = BigDecimal.valueOf(Double.MIN_VALUE),
+        val bound: BigDecimal = BigDecimal.valueOf(Double.MAX_VALUE)
 ) : Arbitrary<BigDecimal>(BigDecimalGen(origin, bound), BigDecimalShrinker(origin, bound))
 
 class BigIntegerArbitrary(
-        val origin: BigInteger,
-        val bound: BigInteger
+        val origin: BigInteger = BigInteger.valueOf(Long.MIN_VALUE),
+        val bound: BigInteger = BigInteger.valueOf(Long.MAX_VALUE)
 ) : Arbitrary<BigInteger>(BigIntegerGen(origin, bound), BigIntegerShrinker(origin, bound))
 
-class OneOfArbitrary<T>(values: List<T>) : Arbitrary<T>(OneOfGen(values), OneOfShrinker(values)) {
+class OneOfArbitrary<T>(values: List<T>) : Arbitrary<T>(OneOfGen(values), OneOfShrinker(values))
 
-    constructor(values: Array<T>) : this(values.asList())
+class OneOfGenArbitrary<T>(gens: List<Gen<T>>) : Arbitrary<T>(OneOfGenGen(gens))
 
-    constructor(values: Set<T>) : this(values.toList())
-}
+/** API */
 
-class OneOfGenArbitrary<T>(gens: List<Gen<T>>) : Arbitrary<T>(OneOfGenGen(gens)) {
+fun longs(
+        origin: Long = Long.MIN_VALUE,
+        bound: Long = Long.MAX_VALUE
+): LongArbitrary = LongArbitrary(origin = origin, bound = bound)
 
-    constructor(values: Array<Gen<T>>) : this(values.asList())
+fun ints(
+        origin: Int = Int.MIN_VALUE,
+        bound: Int = Int.MAX_VALUE
+): IntArbitrary = IntArbitrary(origin = origin, bound = bound)
 
-    constructor(values: Set<Gen<T>>) : this(values.toList())
-}
+fun shorts(
+        origin: Short = Short.MIN_VALUE,
+        bound: Short = Short.MAX_VALUE
+): ShortArbitrary = ShortArbitrary(origin = origin, bound = bound)
+
+fun bytes(
+        origin: Byte = Byte.MIN_VALUE,
+        bound: Byte = Byte.MAX_VALUE
+): ByteArbitrary = ByteArbitrary(origin = origin, bound = bound)
+
+fun chars(
+        origin: Char = Random.PRINTABLE_ASCII_ORIGIN.toChar(),
+        bound: Char = Random.PRINTABLE_ASCII_BOUND.toChar()
+): CharArbitrary = CharArbitrary(origin = origin, bound = bound)
+
+fun booleans(): BooleanArbitrary = BooleanArbitrary()
+
+fun doubles(
+        origin: Double = Double.MIN_VALUE,
+        bound: Double = Double.MAX_VALUE
+): DoubleArbitrary = DoubleArbitrary(origin = origin, bound = bound)
+
+fun floats(
+        origin: Float = Float.MIN_VALUE,
+        bound: Float = Float.MAX_VALUE
+): FloatArbitrary = FloatArbitrary(origin = origin, bound = bound)
+
+fun bigDecimals(
+        origin: BigDecimal = BigDecimal.valueOf(Double.MIN_VALUE),
+        bound: BigDecimal = BigDecimal.valueOf(Double.MAX_VALUE)
+): BigDecimalArbitrary = BigDecimalArbitrary(origin = origin, bound = bound)
+
+fun bigInts(
+        origin: BigInteger = BigInteger.valueOf(Long.MIN_VALUE),
+        bound: BigInteger = BigInteger.valueOf(Long.MAX_VALUE)
+): BigIntegerArbitrary = BigIntegerArbitrary(origin = origin, bound = bound)
+
+fun <T> const(constant: T): Arbitrary<T> = Arbitrary(object : Gen<T> {
+    override fun generate(): T = constant
+
+    override fun isAcceptable(value: T): Boolean = value == constant
+})
+
+fun <T> oneOf(values: List<T>): OneOfArbitrary<T> = OneOfArbitrary<T>(values)
+
+fun <T> oneOf(vararg values: T): OneOfArbitrary<T> = OneOfArbitrary<T>(values.asList())
+
+fun <T> oneOf(values: Set<T>): OneOfArbitrary<T> = OneOfArbitrary<T>(values.toList())
+
+fun <T> oneOf(gens: List<Gen<T>>): OneOfGenArbitrary<T> = OneOfGenArbitrary<T>(gens)
+
+fun <T> oneOf(vararg gens: Gen<T>): OneOfGenArbitrary<T> = OneOfGenArbitrary<T>(gens.asList())
+
+fun <T> oneOf(gens: Set<Gen<T>>): OneOfGenArbitrary<T> = OneOfGenArbitrary<T>(gens.toList())
